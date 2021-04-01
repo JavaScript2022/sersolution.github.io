ab58a0eaada0ac7ade80509079344c499019e574
JavaScript2022 <77944956+JavaScript2022@users.noreply.github.com>
2021-04-01 18:59:40 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,35 @@ $( document ).ready(function(){
       $('body').toggleClass('lock');
     });
   });
+
+
+
+
+  const menuLinks = document.querySelectorAll('.header__link[data-goto]');
+  if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+      menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+      const menuLink = e.target;
+      if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+          const gotoBlock = document.querySelector(menuLink.dataset.goto);
+          const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+          window.scrollTo({
+            top: gotoBlockValue,
+            behavior: "smooth"
+          });
+          e.preventDefault();
+      }
+    }
+}
+
+
+
+
+
+window.addEventListener("load",function(){
+  document.getElementById("loader").classList.toggle("loader2")
+})
